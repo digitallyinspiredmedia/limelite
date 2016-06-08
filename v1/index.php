ca4062f1693410b3d5cd856c33bb06ecbadd4829
sikavinraj <sikavinraj@gmail.com>
2016-06-08 18:28:37 +0530

    sms configured

--- v1/index.php
+++ v1/index.php
@@ -35,12 +35,6 @@
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="message" class="col-sm-2 control-label">Message</label>
-						<div class="col-sm-10">
-							<textarea class="form-control" rows="4" name="message"></textarea>
-						</div>
-					</div>
-					<div class="form-group">
 						<div class="col-sm-10 col-sm-offset-2">
 							<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
 						</div>
@@ -54,66 +48,5 @@
 	</div>   
   
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-<script>
-$(function() {
-
-	// Get the form.
-	var form = $('#ajax-contact');
-
-	// Get the messages div.
-	var formMessages = $('#form-messages');
-
-	// Set up an event listener for the contact form.
-	$(form).submit(function(e) {
-		// Stop the browser from submitting the form.
-		e.preventDefault();
-
-		// Serialize the form data.
-		var formData = $(form).serialize();
-
-		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
-
-			// Set the message text.
-			$(formMessages).text(response);
-
-			// Clear the form.
-			$('#name').val('');
-			$('#number').val('');
-			$('#email').val('');
-			$('#message').val('');
-
-			//test
-			$('#accountSid').val('');
-			$('#password').val('');
-			$('#to').val('');
-			$('#body').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
-
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
-		});
-
-	});
-
-});
-</script>
-
 </body>
 </html>
--- v1/sms.php
+++ v1/sms.php
@@ -1,7 +1,46 @@
 <?php
+//db config
+define('DB_SERVER', 'limelite.db.9731563.hostedresource.com');
+define('DB_USERNAME', 'limelite');    // DB username
+define('DB_PASSWORD', 'Lime@123');    // DB password
+define('DB_DATABASE', 'limelite');      // DB name
+$connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
+$database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
+
+/*
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `number` varchar(60) NOT NULL,
+  `message` varchar(60) NOT NULL,
+  PRIMARY KEY (`ID`)
+);
+ header("Location: ".$loginUrl);
+
+*/
+
+//$ID = $_POST['ID'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$number = $_POST['number'];
+//$message = $_POST['message'];
+
+
+
+
+//function checkuser($ID,$name,$email,$number,$message){
+
+ $check = mysql_query("select * from users where email='$email'");
+	$check = mysql_num_rows($check);
+	if (empty($check)) { // if new user . Insert a new record
+	$query = "INSERT INTO users (name,email,number,message) VALUES ('$name','$email','$number','$message')";
+	mysql_query($query);
+
+//sms
 $request =""; //initialise the request variable
 $param[method]= "sendMessage";
-$param[send_to] = "917811811767";
+$param[send_to] = $number;
 $param[msg] = "Dear Patron, We are Celebrating our 1st anniversary at LIMELITE-Race course! Avail flat 30% off on services. Hurry, Ltd period offer. Call 0422- 4202006 for appt.";
 $param[msg_type] = "TEXT"; //Can be "FLASH”/"UNICODE_TEXT"/”BINARY”
 $param[userid] = "2000142572";
@@ -27,5 +66,16 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $curl_scraped_page = curl_exec($ch);
 curl_close($ch);
-echo $curl_scraped_page;
+
+	echo ' :) ' ;
+	} else {   // If Returned user . update the user record
+	$query = "INSERT INTO users (name,email,number,message) VALUES ('$name','$email','$number','$message')";
+	mysql_query($query);
+	echo ' :) ' ;
+	}
+	echo $curl_scraped_page;
+//}
+
+
+
 ?>
